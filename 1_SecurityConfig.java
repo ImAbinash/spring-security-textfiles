521e8c89ca156a34e8779bf48fb8ec438e665e07
Abinash Pradhan <37651464+ImAbinash@users.noreply.github.com>
2022-07-28 08:53:13 +0530

    SecurityConfig with default password encoder and nooppassword encoder.
    InMemoryUserDetailsManager with default password encoders

---
+++ 1_SecurityConfig.java
@@ -0,0 +1,60 @@
+package com.example.authpoc.config;
+
+import java.util.function.Function;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+	@Bean
+	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests((auth) -> auth.antMatchers("/register","/home").permitAll().anyRequest()
+				.authenticated()).httpBasic(Customizer.withDefaults());
+		return http.build();
+	}
+/*
+// This doesn't contain default password encode hence we created nopassword encoder 
+// For default password encode see the bellow commented section 
+
+
+	@Bean
+	public InMemoryUserDetailsManager userDetailsService() {
+		UserDetails userDetails = User.withUsername("ImAbinash").password("test").authorities("Admin").build();
+		return new InMemoryUserDetailsManager(userDetails);
+	}
+	
+	@Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+	*/
+  //  --------------------------------- OR ---------------------------------
+  // With default password encoder
+  /*
+  
+  // This is using default password encoder which is bcrypt password encode
+  
+  @Bean
+	public InMemoryUserDetailsManager userDetailsService() {
+		UserDetails userDetails = User.withDefaultPasswordEncoder().username("ImAbinash").password("test").authorities("Admin").build();
+		return new InMemoryUserDetailsManager(userDetails);
+	}
+  
+  */
+  
+  
+  
+  
+	
+}
